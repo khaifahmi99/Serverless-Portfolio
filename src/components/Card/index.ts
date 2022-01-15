0c433b24ff1f463e81240c625d17b2da29b6f7d0
Khai Fahmi <khaifahmi99@gmail.com>
2022-01-15 09:34:51 +0000

    implemented small views for experiences section

--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -1,2 +1,3 @@
 export { default } from './Card';
 export { default as QuoteCard } from './QuoteCard';
+export { default as InformationCard } from './InformationCard';
--- src/views/Experience/Experience.tsx
+++ src/views/Experience/Experience.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { ChevronDoubleLeftIcon, CheckCircleIcon } from '@heroicons/react/solid';
-import { ExternalLinkIcon } from '@heroicons/react/outline';
+import { ChevronDoubleLeftIcon } from '@heroicons/react/solid';
 
-import Card from "../../components/Card";
+import { InformationCard } from "../../components/Card";
 import Title from "../../components/Title";
+import { Duration } from "../../components/Card/InformationCard";
 
 const Experience = (): JSX.Element => {
     // TODO: Get from JSON
@@ -57,48 +57,28 @@ const Experience = (): JSX.Element => {
     return (
         <div className='max-w-7xl mx-auto sm:px-6 lg:px-8 py-8'>
             <Title text='Experiences' />
+            <div className='flex flex-col lg:hidden'>
+                {EXPERIENCES.slice(0, 2).map((exp, idx) => (
+                    <div id={`md:exp-${idx}`} className='h-96 w-full mb-4'>
+                        <InformationCard
+                            link={exp.link}
+                            meta={exp.technologies}
+                            title={`${exp.role}@${exp.company}`}
+                            timePeriod={(exp.duration as Duration)}
+                            highlights={exp.hightlights}
+                        />
+                    </div>
+                ))}
+            </div>
             <div className='flex flex-row space-x-4 w-full h-96'>
                 <div className='w-3/4 hidden lg:flex'>
-                    <Card>
-                        <div className='flex flex-col text-left w-full h-full p-12 text-white'>
-                            <p>{activeExperience.duration[0]}{activeExperience.duration[1] ? ` - ${activeExperience.duration[1]}` : ''}</p>
-                            <div className='flex flex-row justify-between'>
-                                <h1 className='font-bold text-lg'>{activeExperience.role}@{activeExperience.company}</h1>
-                                {activeExperience.link && (
-                                    <ExternalLinkIcon 
-                                        className={`h-8 w-8 ml-2 text-white hover:text-gray-200`} 
-                                        onClick={() => window.open(activeExperience.link, '_blank')}
-                                    />
-                                )}
-                            </div>
-                            <hr className='my-4' />
-                            <div className='flex flex-col justify-between h-3/4'>
-                                <div className='overflow-auto'>
-                                    {activeExperience.hightlights.map((hightlight, idx) => (
-                                        <div 
-                                            id={`hightlight-${idx}`} 
-                                            className='flex flex-row'
-                                        >
-                                            <div className='flex my-auto'>
-                                                <CheckCircleIcon className={`h-6 w-6 mr-2 text-white`}/>
-                                            </div>
-                                            <p>{hightlight}</p>
-                                        </div>
-                                    ))}
-                                </div>
-                                <div className='flex flex-nowrap justify-end mt-4'>
-                                    {activeExperience.technologies.slice(0, 5).map((tech, idx) => (
-                                        <span
-                                            id={`tech-${idx}`} 
-                                            className='px-2 py-1 bg-white text-indigo-600 ml-2 rounded-lg truncate'
-                                        >
-                                            {tech}
-                                        </span>
-                                    ))}                                
-                                </div>
-                            </div>
-                        </div>
-                    </Card>
+                    <InformationCard 
+                        link={activeExperience.link}
+                        meta={activeExperience.technologies}
+                        title={`${activeExperience.role}@${activeExperience.company}`}
+                        timePeriod={(activeExperience.duration as Duration)}
+                        highlights={activeExperience.hightlights}
+                    />
                 </div>
                 <div className='hidden lg:block w-1/2 overflow-y-auto border-t-2 border-indigo-100'>
                     {EXPERIENCES.map((exp, idx) => (
