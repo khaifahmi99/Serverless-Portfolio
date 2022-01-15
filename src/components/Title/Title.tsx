03369e4399ee20fd1c4c208e730db9117b6d4dfe
Khai Fahmi <khaifahmi99@gmail.com>
2022-01-15 01:42:46 +0000

    fix line in Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,26 +2,20 @@ import { FC } from "react";
 
 export interface TitleProps {
     text: string;
-    twOptions?: {
-        backgroundColor?: string;
-    }
 }
 
 const Title: FC<TitleProps> = ({ 
     text, 
-    twOptions = { backgroundColor: 'white' }, 
 }) => (
-    <div className='pb-14 pt-14'>
+    <div className='pb-14 pt-14 flex flex-row'>
         <h1 
-            className={`text-indigo-600 text-6xl tracking-tight font-bold md:border-b-4 border-gray-300 border-0`}
-            style={{ lineHeight: '0.1em' }}
+            className={`text-indigo-600 text-6xl tracking-tight font-bold pr-4 pb-1`}
         >
-            <span 
-                className={`bg-${twOptions.backgroundColor} pr-4`}
-            >
-                {text}
-            </span>
+            {text}
         </h1>
+        <h1 
+            className='w-full h-[2.2em] border-b-4 border-gray-300'
+        ></h1>
     </div>
 )
 
--- src/views/Experience/Experience.tsx
+++ src/views/Experience/Experience.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ChevronDoubleLeftIcon } from '@heroicons/react/solid'
+
 import Card from "../../components/Card";
 import Title from "../../components/Title";
 
@@ -6,18 +8,21 @@ const Experience = (): JSX.Element => {
     // TODO: Get from JSON
     const EXPERIENCES = [
         {
+            id: '1',
             role: 'Intern',
             company: 'Company A',
             technologies: ['HTML', 'CSS'],
             duration: [2018, 2019],
         },
         {
+            id: '2',
             role: 'Junior',
             company: 'Company B',
             technologies: ['Linux', 'Python'],
             duration: [2019, 2021],
         },
         {
+            id: '3',
             role: 'Mid',
             company: 'Company C',
             technologies: ['TailwindCSS', 'AWS'],
@@ -28,19 +33,22 @@ const Experience = (): JSX.Element => {
     // TODO: must check before accessing index 0
     const [activeExperience, setActiveExperience] = React.useState(EXPERIENCES[0]);
 
-    const latestExperience = EXPERIENCES.length > 4 ? EXPERIENCES.slice(0, 3) : EXPERIENCES;
-
     return (
         <div className='max-w-7xl mx-auto sm:px-6 lg:px-8 py-8'>
             <Title text='Experiences' />
             <div className='flex flex-row space-x-4 w-full h-96'>
-                <div className='w-full'>
+                <div className='w-full hidden lg:flex'>
                     <Card>{activeExperience.role}@{activeExperience.company}</Card>
                 </div>
-                <div className='flex flex-col space-y-4 w-1/2 overflow:auto'>
-                    {latestExperience.map((exp, idx) => (
-                        <div id={`exp-${idx}`} className='h-16' onClick={() => setActiveExperience(exp)}>
-                            <Card>{exp.role}@{exp.company}</Card>
+                <div className='hidden lg:block w-1/2 overflow-y-auto border-t-2 border-indigo-100'>
+                    {EXPERIENCES.map((exp, idx) => (
+                        <div 
+                            id={`exp-${idx}`} 
+                            className='flex flex-row h-16 py-4 border-b-2 border-indigo-100' 
+                            onClick={() => setActiveExperience(exp)}
+                        >
+                            <ChevronDoubleLeftIcon className={`h-6 w-6 mr-4 text-indigo-400`}/>
+                            <p className={`text-center text-gray-600 ${exp.id === activeExperience.id ? 'font-bold' : ''} w-full`}>{exp.role}@{exp.company}</p>
                         </div>
                     ))}
                 </div>
